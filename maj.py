3836814f7c713e3c9e4057ef29637e58c93456ec
Fourati Mohamed <80990137+FouratiMohamed@users.noreply.github.com>
2023-10-13 15:39:30 +0100

    Create maj.py

---
+++ maj.py
@@ -0,0 +1,38 @@
+# fichier vers tableau
+def fichierVersTableau(nomFichier):
+    f = open(nomFichier, "rb")
+    T = array([{} for i in range(100)])
+    n = 0
+    try:
+        while True:
+            livre = load(f)
+            T[n] = livre
+            n += 1
+    except EOFError:
+        f.close()
+    return T[:n], n
+
+# tableau vers fichier
+def tableauVersFichier(T, nomFichier):
+    f = open(nomFichier, "wb")
+    for i in range(len(T)):
+        dump(T[i], f)
+    f.close()
+        
+# mise a jour du disponibilite d'un livre
+def maj():
+    afficher()
+    ref = int(input("Reference: "))
+    estAjout = input("Ajouter ou retirer (a/r): ")
+    T,n = fichierVersTableau("livres.dat")
+    i = 0
+    while i < n and T[i]["ref"] != ref:
+        i += 1
+    if i < n:
+        if estAjout == "a":
+            T[i]["disp"] += 1
+        else:
+            T[i]["disp"] -= 1
+        tableauVersFichier(T, "livres.dat")
+    else:
+        print("Reference invalide")
